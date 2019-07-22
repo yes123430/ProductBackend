f0b45788ec3fe4d9d2614dbe910685138b4141b0
yes123430@gmail.com <yes123430@gmail.com>
2019-07-22 21:12:12 +0800

    change bootstrap

---
+++ src/ProductBackend/Controllers/ShopController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProductBackend.EF;
+using ProductBackend.Models;
+
+namespace ProductBackend.Controllers
+{
+    public class ShopController : Controller
+    {
+        private DBModelContext db = new DBModelContext();
+
+        // GET: Shop
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Products.ToListAsync());
+        }
+
+        // GET: Shop/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductModel productModel = await db.Products.FindAsync(id);
+            if (productModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productModel);
+        }
+
+        // GET: Shop/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shop/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,ProdName,ImagePath,Price,Count")] ProductModel productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(productModel);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(productModel);
+        }
+
+        // GET: Shop/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductModel productModel = await db.Products.FindAsync(id);
+            if (productModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productModel);
+        }
+
+        // POST: Shop/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,ProdName,ImagePath,Price,Count")] ProductModel productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productModel).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(productModel);
+        }
+
+        // GET: Shop/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductModel productModel = await db.Products.FindAsync(id);
+            if (productModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productModel);
+        }
+
+        // POST: Shop/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ProductModel productModel = await db.Products.FindAsync(id);
+            db.Products.Remove(productModel);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
